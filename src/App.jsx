c1624f77991a124561a6514123f085a8730bb873
Arjun <arjundasback515@gmail.com>
2023-08-26 14:56:55 +0600

    left side button and bank detail design added done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-
 import "./App.css";
+import LeftSide from "./Component/LeftSide/LeftSide";
 
 function App() {
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      
+      <div className="md:flex justify-between p-3 max-w-screen-xl mx-auto md:my-20 my-5">
+        {/* left site button  */}
+        <div>
+        <LeftSide></LeftSide>
+        </div>
+        {/* rihgt side button  */}
+        <div>
+          <h1>hello2</h1>
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/Component/LeftSide/LeftSide.jsx
@@ -0,0 +1,68 @@
+import logo from "../../assets/cimb-bank.png";
+import logo1 from "../../assets/bank-one.png";
+import bank from "../../assets/bank.png";
+
+const LeftSide = () => {
+  return (
+    <div>
+      <div className="flex">
+        <button className="btn btn-primary md:mr-7 mr-3">
+          Total amount: $5870
+        </button>
+        <button className="btn btn-primary">Pay amount: $200</button>
+      </div>
+      <div className="mt-7 flex items-center">
+        <button className="btn btn-primary md:mr-7 mr-3">
+          Select Payment Method
+        </button>
+        <div className="flex flex-col-reverse">
+          <button className="btn btn-primary md:mr-7 mr-3  btn-sm">Bank</button>
+          <button className="btn btn-primary md:mr-7 mr-3  btn-sm mb-3">
+            Mobile E-Wallet
+          </button>
+        </div>
+      </div>
+      <div className="mt-8 flex items-center ">
+        <button className="btn btn-primary md:mr-7 mr-3  btn-sm mb-3">
+          Bank Name
+        </button>
+        <div className="flex flex-col-reverse ">
+          <img className="w-20 mt-2" src={logo} alt="" />
+          <img className="w-20" src={logo1} alt="" />
+        </div>
+      </div>
+      <div className="mt-10 lg:flex items-center">
+        <div>
+          <img className="w-40 mx-auto" src={bank} alt="" />
+        </div>
+        <div className="md:mx-8 mt-5">
+          <h1 className="text-center">Virtual A/C Now : 20200002020</h1>
+          <h1 className="text-center">Virtual A/C Now : 20200002020</h1>
+          <h1 className="text-center">Virtual A/C Now : 20200002020</h1>
+          <h1 className="text-center">Virtual A/C Now : 20200002020</h1>
+          <h1 className="text-center">Virtual A/C Now : 20200002020</h1>
+        </div>
+      </div>
+      <div className="mt-8 text-center">
+        <button className="btn btn-primary btn-sm   md:mr-7 mr-3">
+          Remittance bank details
+        </button>
+      </div>
+      <div className="mt-8">
+        <button className="btn btn-primary btn-md   md:mr-7 mr-3">
+          Bank Details
+        </button>
+        <div className="mx-5 mt-5">
+        <h1><span className="font-bold">Owner Name </span>: Eva</h1>
+        <h1><span className="font-bold">Bank Name </span>: Sonali Bank</h1>
+        <h1><span className="font-bold">Account  </span>: Eva</h1>
+        <h1><span className="font-bold">Owner Name </span>: Eva</h1>
+        <h1><span className="font-bold">Owner Name </span>: Eva</h1>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LeftSide;
