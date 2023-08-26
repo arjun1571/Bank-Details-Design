b5a28b66dce097288c3574c40b98a49bd200003b
Arjun <arjundasback515@gmail.com>
2023-08-26 15:26:12 +0600

    right side flag added on ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import "./App.css";
 import LeftSide from "./Component/LeftSide/LeftSide";
+import Right from "./Component/LeftSide/RightSide/Right";
 
 function App() {
   return (
     <>
-      <div className="md:flex justify-between p-3 max-w-screen-xl mx-auto md:my-20 my-5">
+      <div className="md:flex justify-between p-3 max-w-screen-lg mx-auto md:my-20">
         {/* left site button  */}
-        <div>
+        <div className="mb-10">
         <LeftSide></LeftSide>
         </div>
         {/* rihgt side button  */}
-        <div>
-          <h1>hello2</h1>
+        <div className="">
+          <Right></Right>
         </div>
       </div>
     </>
---
+++ src/Component/LeftSide/RightSide/Right.jsx
@@ -0,0 +1,92 @@
+import flag from "../../../assets/flag.svg";
+import bank from "../../../assets/bank.png";
+import up from "../../../assets/up.svg";
+
+const Right = () => {
+  return (
+    <div className="">
+      <div className="flex">
+        <button className="btn btn-primary md:mr-7 mr-3">
+          Total amount: $5870
+        </button>
+        <button className="btn btn-primary">Pay amount: $200</button>
+      </div>
+      <div className="divider"></div>
+      <div className="mt-7 flex items-center">
+        <button className="btn btn-primary md:mr-7 mr-3">
+          Select Payment Method
+        </button>
+        <div className="flex flex-col-reverse">
+          <button className="btn btn-primary md:mr-7 mr-3  btn-sm">Bank</button>
+          <button className="btn btn-primary md:mr-7 mr-3  btn-sm mb-3">
+            Mobile E-Wallet
+          </button>
+        </div>
+      </div>
+      <div className="mt-8 md:flex items-center ">
+        <button className="btn btn-primary md:mr-7 mr-3  btn-sm mb-3">
+          Country
+        </button>
+        <div className="flex items-center ">
+          <img className="w-20 mt-2" src={flag} alt="" />
+          <img className="w-20 mt-2 mx-2" src={flag} alt="" />
+          <img className="w-20 mt-2" src={flag} alt="" />
+        </div>
+      </div>
+      <div className="mt-10 lg:flex items-center">
+        <div>
+          <img className="w-40 mx-auto" src={bank} alt="" />
+        </div>
+        <div className="md:mx-8 mt-5">
+          <h1 className="text-center">Virtual A/C Now : 20200002020</h1>
+          <h1 className="text-center">Virtual A/C Now : 20200002020</h1>
+          <h1 className="text-center">Virtual A/C Now : 20200002020</h1>
+          <h1 className="text-center">Virtual A/C Now : 20200002020</h1>
+          <h1 className="text-center">Virtual A/C Now : 20200002020</h1>
+        </div>
+      </div>
+      <div className="mt-8 text-center">
+        <button className="btn btn-primary btn-sm   md:mr-7 mr-3">
+          Remittance bank details
+        </button>
+      </div>
+      <div className="mt-8">
+        <button className="btn btn-primary btn-md   md:mr-7 mr-3">
+          Bank Details
+        </button>
+        <div className="mx-5 mt-5">
+          <h1>
+            <span className="font-bold">Owner Name </span>: Eva
+          </h1>
+          <h1>
+            <span className="font-bold">Bank Name </span>: Sonali Bank
+          </h1>
+          <h1>
+            <span className="font-bold">Account Num </span>: 4568785445525
+          </h1>
+          <h1>
+            <span className="font-bold">Brance Name </span>: Dhaka
+          </h1>
+          <h1>
+            <span className="font-bold">Routing Number </span>: 574548754654
+          </h1>
+        </div>
+      </div>
+      <div className="mt-5">
+        <div className="flex items-center md:mx-20 mx-10">
+          <button className="btn btn-primary btn-sm   md:mr-7 mr-3">
+            Payment Recept document
+          </button>
+          <img className="w-14" src={up} alt="" />
+        </div>
+      </div>
+      <div className="mt-5 text-center">
+        <button className="btn btn-primary w-60  btn-sm md:mr-7 mr-3">
+          submit
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Right;
